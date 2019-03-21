9340dda680d980d7421579098ff08302efbf5b46
Luke Menard <lukemenard@gmail.com>
2019-03-21 19:49:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,9 +2,10 @@ var playlist = {}
 playlist['Slowdive'] = 'Alison'
 playlist['My Bloody Valentine'] = 'Sometimes'
 
+
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, artistName, songTitle)
-}
+  playlist[artistName] = songTitle
+  return playlist
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
 
