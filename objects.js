57812ae01149669d532840b6a942740afd142e32
Luke Menard <lukemenard@gmail.com>
2019-03-21 19:53:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
 }
 removeFromPlaylist(playlist, 'Slowdive')
